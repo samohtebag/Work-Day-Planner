7ccab3bd7f771ca5c35c060ebf1246f5d7bc0aae
gabe thomas <gabe.thomas@bizzabo.com>
2021-03-11 22:21:23 -0500

    first push to repo. HTML and CSS is done, JS incompleted

---
+++ assets/script.js
@@ -0,0 +1,14 @@
+var today = moment();
+$("#currentDay").text(today.format("MMMM Do YYYY"));
+
+// set interval to update time in real-time, 1000
+
+// make a function that stores the user text content when they press the save button
+// get local storage and set item, get item and set item
+// make a variable for what you are taking from the text box and the time that you have
+// call the colors (classList) for things that have passed, current items, and future items
+// when you call a class/id made on the CSS, use jQuery method .addClass();
+
+// save button -> use siblings and parents to get the var for the text/time
+// moment.js -> 
+// when you get current time, specify in hours to make the function easier (military time is a hint)
